1b5ab7e17147dd79577f1f8fb4d780d482ecc0aa
Robert J Michaels <rgmichaels@gmail.com>
2026-04-08 16:41:30 -0400

    PWF-17 format navigation steps for CI

--- src/steps/navigation.steps.ts
+++ src/steps/navigation.steps.ts
@@ -142,18 +142,15 @@ Then('I should land on the pharmacy page', async function (this: CustomWorld) {
   await this.homePage.expectPharmacyPage();
 });
 
-Then(
-  'I should land on a contact us page',
-  async function (this: CustomWorld) {
-    if (!this.homePage) {
-      throw new Error(
-        'HomePage is not initialized. Open the homepage before validating destination page.'
-      );
-    }
-
-    await this.homePage.expectContactUsPage();
+Then('I should land on a contact us page', async function (this: CustomWorld) {
+  if (!this.homePage) {
+    throw new Error(
+      'HomePage is not initialized. Open the homepage before validating destination page.'
+    );
   }
-);
+
+  await this.homePage.expectContactUsPage();
+});
 
 Then(
   'the page should display {string} text',
